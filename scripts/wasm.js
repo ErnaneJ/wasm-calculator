7e1a4bbaeedc78b82158e85dc3324253032b6c97
Ernane Ferreira <ernane.junior25@gmail.com>
2024-04-30 00:46:32 -0300

    feat: add loader

---
+++ scripts/wasm.js
@@ -0,0 +1,29 @@
+const wasmMemory = new WebAssembly.Memory({ initial: 256, maximum: 256 });
+const wasmTable = new WebAssembly.Table({ initial: 1, maximum: 1, element: 'anyfunc' });
+
+const asmLibraryArg = { 
+  "__handle_stack_overflow": () => {},
+  "emscripten_resize_heap": () => {},
+  "__lock": () => {}, 
+  "__unlock": () => {},
+  "memory": wasmMemory, 
+  "table": wasmTable 
+};
+
+const wasmInfo = { 'env': asmLibraryArg, 'wasi_snapshot_preview1': asmLibraryArg };
+
+async function loadWasm(filePath = 'main.wasm') {
+  try {
+    const response = await fetch(filePath);
+    const bytes    = await response.arrayBuffer();
+    const wasmObj  = await WebAssembly.instantiate(bytes, wasmInfo);
+    return wasmObj.instance.exports;
+  } catch (error) {
+    console.error('Error loading WASM:', error);
+    return null;
+  }
+}
+
+(async () => {
+  window.wasmExports = await loadWasm('../build/arithmetic.wasm');
+})();
